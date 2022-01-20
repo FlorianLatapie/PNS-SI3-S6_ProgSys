29f945fbc452a86019a7e4b8f917f6554efcefd4
Florian LATAPIE <florian.latapie@etu.unice.fr>
2022-01-20 08:17:27 +0100

    td1 rendu

---
+++ td01_distrib/Latapie TD1/td01/debugSimple.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    int z =  argc;
+    char *prog = argv[0];
+    char *arg = argv[1];
+
+    int *w = &z;
+    
+    printf("bonjour\n");
+}
---
+++ td01_distrib/Latapie TD1/td01/memoire.c
@@ -0,0 +1,10 @@
+int main(int argc, char *argv[])
+{
+    int tab[] = {12,45,67,789};
+    int *ii=tab;
+    short *si=tab;
+    char *ci=tab;
+    ii++;
+    si++;
+    ci++;
+}
