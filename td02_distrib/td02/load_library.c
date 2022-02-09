a2ea2991756b1b41b181a3b71327ec977f05fa72
Florian Latapie <florian.latapie@etu.unice.fr>
2022-02-09 10:09:20 +0100

    exo 7

---
+++ td02_distrib/td02/load_library.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <dlfcn.h>
+
+#include "sort.h"
+
+static void (*sortlib)(int [], int);
+
+void sort_from_lib(int tab[], int size){
+    sortlib(tab,size);
+}
+
+void load_library(char *library_name){
+    void *handle;
+    char *error;
+
+    handle = dlopen(library_name, RTLD_LAZY);
+    if (!handle) {
+        fprintf(stderr, "%s\n", dlerror());
+        exit(EXIT_FAILURE);
+    }
+
+    dlerror();    /* Clear any existing error */
+
+    sortlib = (void(*) (int[], int)) dlsym(handle, "sort");
+
+    error = dlerror();
+    if (error != NULL) {
+        fprintf(stderr, "%s\n", error);
+        exit(EXIT_FAILURE);
+    }
+}
---
+++ td02_distrib/td02/load_library.h
@@ -0,0 +1,3 @@
+void sort_from_lib(int list[], int size);
+
+void load_library(char *library_name);
