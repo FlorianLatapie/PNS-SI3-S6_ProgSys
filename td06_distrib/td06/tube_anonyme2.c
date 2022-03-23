1daecb2840ec0efb76c0f8a99920760aaf85c7d8
Florian Latapie <florian.latapie@etu.unice.fr>
2022-03-23 11:02:29 +0100

    exo4

---
+++ td06_distrib/td06/tube_anonyme2.c
@@ -0,0 +1,34 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void lecture(int fd) {
+    char buff[1];
+    while (read(fd, buff, 1)) {
+        write(STDOUT_FILENO, buff, sizeof(buff));
+    }
+    printf("\n");
+}
+
+int main() {
+    int p[2];
+    pipe(p);
+    switch (fork())
+    {
+    case -1:
+        perror("Erreur fork");
+        abort();
+    case 0:
+        sleep(2);
+        write(p[1], "0123456789", sizeof("0123456789"));
+        close(p[1]);
+        break;
+    default:
+        close(p[1]);
+        lecture(p[0]);
+        close(p[0]);
+        break;
+    }   
+
+    return 0;
+}
