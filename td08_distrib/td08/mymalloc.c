39d9b68fa4f1c852dc484795de39cd3ba82fa00e
Florian Latapie <florian.latapie@etu.unice.fr>
2022-04-06 11:17:06 +0200

    Update mymalloc.c

--- td08_distrib/td08/mymalloc.c
+++ td08_distrib/td08/mymalloc.c
@@ -18,56 +18,59 @@
 //
 // ======================================================================
 
-
 // Les fonctions internes à implémenter
 void *internal_malloc(size_t size);
-void  internal_free(void *ptr);
+void internal_free(void *ptr);
 void *internal_calloc(size_t nmemb, size_t size);
 void *internal_realloc(void *ptr, size_t size);
 
-
-// Les fonctions externes: ces fonctions appellent les fonctions internes 
-//  que vous avez à écrire en y ajoutant une trace qui est utilisée par 
-//  to-html et villoc.py pour générer les .html 
-void *mysbrk(intptr_t increment) {
+// Les fonctions externes: ces fonctions appellent les fonctions internes
+//  que vous avez à écrire en y ajoutant une trace qui est utilisée par
+//  to-html et villoc.py pour générer les .html
+void *mysbrk(intptr_t increment)
+{
   void *ret, *old, *tmp;
   static int first_call = 1;
 
-  if (first_call) {
-    old =sbrk(0);
+  if (first_call)
+  {
+    old = sbrk(0);
     fprintf(stderr, "SYS_brk(0)                    =  %p\n", old);
     first_call = 0;
   }
 
   ret = sbrk(increment);
-  tmp = ret +increment;
+  tmp = ret + increment;
   fprintf(stderr, "SYS_brk(%p)                    = %p\n", tmp, tmp);
   return ret;
 }
 
-void *mymalloc(size_t size) {
+void *mymalloc(size_t size)
+{
   void *ret = internal_malloc(size);
-  fprintf(stderr, "mymalloc(%ld)                     = %p\n",  size, ret);
+  fprintf(stderr, "mymalloc(%ld)                     = %p\n", size, ret);
   return ret;
 }
 
-void myfree(void *ptr) {
+void myfree(void *ptr)
+{
   internal_free(ptr);
   fprintf(stderr, "myfree(%p)                    = <void>\n", ptr);
 }
 
-void *myrealloc(void *ptr, size_t size) {
+void *myrealloc(void *ptr, size_t size)
+{
   void *ret = internal_realloc(ptr, size);
-  fprintf(stderr, "myrealloc(%p, %ld)                     = %p\n",  ptr, size, ret);
+  fprintf(stderr, "myrealloc(%p, %ld)                     = %p\n", ptr, size, ret);
   return ret;
 }
 
-void *mycalloc(size_t nmemb, size_t size) {
+void *mycalloc(size_t nmemb, size_t size)
+{
   void *ret = internal_calloc(nmemb, size);
-  fprintf(stderr, "mycalloc(%ld, %ld)                   = %p\n",  nmemb, size, ret);
+  fprintf(stderr, "mycalloc(%ld, %ld)                   = %p\n", nmemb, size, ret);
   return ret;
 }
-
 
 // ======================================================================
 //
@@ -75,77 +78,96 @@ void *mycalloc(size_t nmemb, size_t size) {
 //
 // ======================================================================
 
-
 /* On suppose qu'on ajoute au moins 50 Headers au minimum */
-#define MIN_ALLOCATION        50
+#define MIN_ALLOCATION 50
 
 /* Pour s'aligner sur des frontieres multiples
  * de la taille du type le plus contraignant
  */
 #define MOST_RESTRICTING_TYPE long double
-typedef union header {                          /* Header de bloc */
-    struct {
-        size_t size;                            /* Taille du bloc */
-        union header *ptr;                      /* bloc libre suivant */
-    } info;
-    MOST_RESTRICTING_TYPE dummy;        /* Ne sert qu'a provoquer un alignement */
+typedef union header
+{ /* Header de bloc */
+  struct
+  {
+    size_t size;       /* Taille du bloc */
+    union header *ptr; /* bloc libre suivant */
+  } info;
+  MOST_RESTRICTING_TYPE dummy; /* Ne sert qu'a provoquer un alignement */
 } Header;
 #define HEADER_SIZE sizeof(Header)
 
-#define NEXT(p)                  ((p)->info.ptr)
-#define SIZE(p)                  ((p)->info.size)
+#define NEXT(p) ((p)->info.ptr)
+#define SIZE(p) ((p)->info.size)
 
 /* L'unité de découpage de la mémoire est la taille de Header car ça permet de gérer facilement chaînage
    Le programme appelant malloc reçoit un pointeur et utilise la mémoire comme il veut.
    BLOCKS_TO_ALLOCATE renvoie le nombre de blocks nécessaires pour le malloc demandé, en tenant compte du header */
 #define BLOCKS_TO_ALLOCATE(size) (1 + (size + HEADER_SIZE - 1) / HEADER_SIZE)
 
-static Header base   = { {0, &base} }; /* Le pointeur de départ: aucune zone allouée */
-static Header *freep = &base;          /* freep pointe sur la 1ère zone libre */
+static Header base = {{0, &base}}; /* Le pointeur de départ: aucune zone allouée */
+static Header *freep = &base;      /* freep pointe sur la 1ère zone libre */
 
 /* ====================================================================== */
 
 /* Utilisé si on n'a pas trouvé assez de place */
 static void *allocate_core(size_t size)
 {
-    // A COMPLETER	
-	// fait un appel à sbrk pour allouer la mémoire et 
-    // un appel à myfree pour chaîner la zone allouée avec la liste freep
-    return freep;
+  Header *new;
+
+  // Allouer au moins MIN_ALLOCATION cellules
+  if (size < MIN_ALLOCATION)
+  {
+    size = MIN_ALLOCATION;
+  }
+
+  new = mysbrk(size * HEADER_SIZE);
+
+  // Si on a pas pu allouer assez de place
+  if (new == (void *)-1)
+  {
+    return NULL;
+  }
+
+  // Mise à jour de la taille du header
+  SIZE(new) = size;
+
+  // Libérer ce bloc (ceci afin de le remettre dans la liste des blocs libres)
+  internal_free(new + 1);
+  
+  // Retourner le pointeur vers le premier élément de la nouvelle zone 
+  return freep;
 }
 
 /* ====================================================================== */
 
 void *internal_malloc(size_t size)
 {
-    // A COMPLETER	
-	// fait un appel à allocate_core si pas de place dans la liste freep
-    return NULL;
+  // A COMPLETER
+  // fait un appel à allocate_core si pas de place dans la liste freep
+  return NULL;
 }
 
 /* ====================================================================== */
 
 void internal_free(void *ptr)
 {
-	// A COMPLETER	
-	// Libère la zone pointée par ptr en l'insérant dans freep
-	// Essai de coller les blocs adjacents à cette zone
-	// NOTA : la liste freep est triée par ordre croissant des adresses
+  // A COMPLETER
+  // Libère la zone pointée par ptr en l'insérant dans freep
+  // Essai de coller les blocs adjacents à cette zone
+  // NOTA : la liste freep est triée par ordre croissant des adresses
 }
 
 /* ====================================================================== */
 
 void *internal_calloc(size_t nmemb, size_t size)
 {
-	// A COMPLETER	
-	// Utilise internal_malloc
+  // A COMPLETER
+  // Utilise internal_malloc
 }
 
 void *internal_realloc(void *ptr, size_t size)
 {
-    // A COMPLETER	
-	// utilise internal_free et internal_malloc
-    return ptr;
-
+  // A COMPLETER
+  // utilise internal_free et internal_malloc
+  return ptr;
 }
-
