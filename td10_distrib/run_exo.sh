13febc7059dcf6686ee0812f6cbab1e99bd02e6c
Florian Latapie <florian.latapie@etu.unice.fr>
2022-04-27 09:41:13 +0200

    Update run_exo.sh

--- td10_distrib/run_exo.sh
+++ td10_distrib/run_exo.sh
@@ -1,3 +1,7 @@
-make
-echo -e "\n--- lancement du programme" exo$1 "---\n"
-java -Djava.library.path=./lib exo$1
+if [ -z "$1" ]; then
+    echo -e "Aucun argument donné\nUsage: ./run_exo.sh <exo_number>"
+else
+    make
+    echo -e "\n--- lancement du programme" exo$1 "---\n"
+    java -Djava.library.path=./lib exo$1
+fi
